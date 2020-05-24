75a4784e27ce637b3792436253a13bcf73ef0c32
62726164 <btilley@gatech.edu>
2020-05-24 14:44:59 -0400

    go pbkdf2 start

--- go/main.go
+++ go/main.go
@@ -1,4 +1,13 @@
 package main
 
+import (
+	"crypto/sha512"
+	"fmt"
+	"golang.org/x/crypto/pbkdf2"
+)
+
 func main() {
+	dk := pbkdf2.Key([]byte("The sentence."), []byte("word"), 32768, 64, sha512.New)
+	fmt.Printf("%v \n", dk)
+	fmt.Printf("%x \n", dk)
 }
